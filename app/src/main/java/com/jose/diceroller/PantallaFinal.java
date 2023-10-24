17c860c7ce1a8f041ee7070e2b2a8db08c40d6db
CGhizzardi <cghizzardi@uoc.edu>
2023-10-24 22:39:29 +0200

    creación de la base de datos y el boton de guardado

--- app/src/main/java/com/jose/diceroller/PantallaFinal.java
+++ app/src/main/java/com/jose/diceroller/PantallaFinal.java
@@ -1,19 +1,29 @@
 package com.jose.diceroller;
 
 import android.annotation.SuppressLint;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.jose.diceroller.db.DbHelper;
 
 public class PantallaFinal extends AppCompatActivity {
 
     private TextView textView;
+    private EditText gamerN;
     private EditText editText;
 
     private TextView txtNombre;
     private GlobalVariables datos;
+    private Button saveName;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -22,8 +32,21 @@ public class PantallaFinal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_final);
         textView = findViewById(R.id.txt_puntos);//localizamos el txt de los puntos
+        gamerN = findViewById(R.id.txtGamerN);
         String puntos = "Monedas ganadas: " + String.valueOf(datos.getPuntuacion());//mostramos el valor de la puntuación global
         textView.setText(puntos);//mensaje de número de monedas
+        saveName = findViewById(R.id.SaveButton);
+
+        saveName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DbHelper dbHelper = new DbHelper(PantallaFinal.this);
+                SQLiteDatabase db =dbHelper.getWritableDatabase();
+                if(db != null){
+                    Toast.makeText(PantallaFinal.this, "Puntuación guardada", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
     }
 
---
+++ app/src/main/java/com/jose/diceroller/db/DbHelper.java
@@ -0,0 +1,34 @@
+package com.jose.diceroller.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DbHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "diceroller.db";
+    public static final String TABLE_PUNTUACION = "puntuacion";
+
+
+
+    public DbHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE "+ TABLE_PUNTUACION + "(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "Nombre TEXT NOT NULL," +
+                "Puntuacón INTEGER NOT NULL)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE " + TABLE_PUNTUACION);
+        onCreate(db);
+    }
+}
