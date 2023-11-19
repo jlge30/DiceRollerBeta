3691b346ac7e3c451262dd84da59dbf3e59b893e
CGhizzardi <cghizzardi@uoc.edu>
2023-11-19 18:09:56 +0100

    creacion de metodo de captura de pantallas

--- app/src/main/java/com/jose/diceroller/MainActivity.java
+++ app/src/main/java/com/jose/diceroller/MainActivity.java
@@ -1,13 +1,21 @@
 package com.jose.diceroller;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.CalendarContract;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -16,11 +24,15 @@ import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.provider.CalendarContract;
+
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
 import java.util.Random;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 //    private TextView resultado1;
@@ -31,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
     private Random random1 = new Random();
     private Random random2 = new Random();
     private TextView txtMensaje;
+
+
 
     private TextView txtPuntuacion, txtFinalJuego;
     private TextView txtTiradas;
@@ -288,5 +302,52 @@ comienza la creación del SoundPool
         }
         super.onDestroy();
     }
+
+    public int getCalendarId(Context context) {
+        Cursor cursor = context.getContentResolver().query(CalendarContract.Calendars.CONTENT_URI,
+                new String[]{CalendarContract.Calendars._ID},
+                CalendarContract.Calendars.VISIBLE + " = 1",
+                null,
+                null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            int columnIndex = cursor.getColumnIndex(CalendarContract.Calendars._ID);
+            if (columnIndex != -1) {
+                return cursor.getInt(columnIndex);
+            } else {
+                // Manejar el error: la columna no existe
+                Log.e("Error", "La columna no existe");
+                return -1;
+            }
+        } else {
+            // Manejar el error: el cursor está vacío o nulo
+            Log.e("Error", "El cursor está vacío o nulo");
+            return -1;
+        }
+    }
+
+    //=============================================================================================
+
+    public void insertEvent() {
+        int dia1, mes1, anno1, hora1, minuto1, dia2, mes2, anno2, hora2, minuto2;
+        final Calendar cal = Calendar.getInstance();
+        final Calendar calendario1 = Calendar.getInstance();
+        final Calendar calendario2 = Calendar.getInstance();
+        dia1 = cal.get(Calendar.DAY_OF_MONTH);
+        mes1 = cal.get(Calendar.MONTH);
+        anno1 = cal.get(Calendar.YEAR);
+        hora1 = cal.get(Calendar.HOUR);
+        minuto1 = cal.get(Calendar.MINUTE);
+        dia2 = dia1 + 1;
+        mes2 = mes1;
+        anno2 = anno1;
+        calendario1.set(Calendar.DAY_OF_MONTH, dia1);
+        calendario1.set(Calendar.MONTH, mes1);
+
+    }
+
+
+
+
 
 }
