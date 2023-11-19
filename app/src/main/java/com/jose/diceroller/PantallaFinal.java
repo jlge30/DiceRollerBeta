262d2612654d27ff2798049aa723359bf6f57806
CGhizzardi <cghizzardi@uoc.edu>
2023-11-19 12:44:11 +0100

    creacion de metodo de captura de pantallas

--- app/src/main/java/com/jose/diceroller/PantallaFinal.java
+++ app/src/main/java/com/jose/diceroller/PantallaFinal.java
@@ -1,10 +1,16 @@
 package com.jose.diceroller;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,12 +20,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.Manifest;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.jose.diceroller.db.DbManager;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -38,8 +52,22 @@ public class PantallaFinal extends AppCompatActivity {
     private TextView txtPuntuacion;
     private GlobalVariables datos;
     private Button saveName, btnSalir, btnInicio;
-
+    View vista;
     private DbManager dbManager;
+
+    private static final int REQUEST_CODE_PERMISO_ESCRIBIR_EXTERNO = 2;
+    private static final int REQUEST_CODE_PERMISO_UBICACION_MEDIOS = 1;
+    private static final int REQUEST_CODE_PERMISO_ESCRIBIR_EXT = 2;
+
+    private static final String[] PERMISSION = {
+            Manifest.permission.ACCESS_MEDIA_LOCATION,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.MEDIA_CONTENT_CONTROL
+    };
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+
+
 
 
 
@@ -56,9 +84,15 @@ public class PantallaFinal extends AppCompatActivity {
         String puntos = "Monedas ganadas: " + String.valueOf(datos.getPuntuacion());//mostramos el valor de la puntuación global
         textView.setText(puntos);//mensaje de número de monedas
         saveName = findViewById(R.id.SaveButton);
+        vista = findViewById(R.id.SaveButton);
         txtPuntuacion = findViewById(R.id.txtScoreTitle);
         btnInicio =findViewById(R.id.btn_volver_jugar);
         btnSalir = findViewById(R.id.btn_salir);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_MEDIA_LOCATION}, REQUEST_CODE_PERMISO_ESCRIBIR_EXTERNO);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISO_ESCRIBIR_EXTERNO);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.MEDIA_CONTENT_CONTROL}, REQUEST_CODE_PERMISO_ESCRIBIR_EXTERNO);
+
+        verifyPermission(this);
         saveName.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
@@ -66,6 +100,7 @@ public class PantallaFinal extends AppCompatActivity {
 
                 if (gamerN.getText().toString().isEmpty()){//comprobamos que la caja de texto nombre tiene datos
                     Toast.makeText(PantallaFinal.this, "Introduce el nombre", Toast.LENGTH_SHORT).show();
+                    createFile(getWindow().getDecorView().getRootView(), "result");
                 }else {
 
                     insertJugadorRx();
@@ -74,6 +109,8 @@ public class PantallaFinal extends AppCompatActivity {
                     textView.setVisibility(View.INVISIBLE);//dejamos invisibles la puntuacion y el texto de título
                     txtPuntuacion.setVisibility(View.INVISIBLE);
                     saveName.setEnabled(false);//deshabilitamos el botón de guardar
+                    //createFile(getWindow().getDecorView().getRootView(), "result");
+                    tomarCapturaDePantalla();
                 }
 
 
@@ -151,6 +188,105 @@ public class PantallaFinal extends AppCompatActivity {
             finish();
         }
         return true;
+    }
+
+    protected static File createFile(View view, String filename) {
+        Date date = new Date();
+        CharSequence format = DateFormat.getDateInstance().format("yyyy-MM-dd_hh:mm:ss");
+        try {
+            String directoryPath = Environment.getExternalStorageDirectory().toString() + "/dashrolls";
+            File directorioDashrolls = new File(Environment.getExternalStorageDirectory(), "dashrolls");
+            if (!directorioDashrolls.exists()) {
+                directorioDashrolls.mkdirs();
+            }
+            String path = directoryPath + "/" + filename + "-" + ".jpg";
+            view.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache());
+            view.setDrawingCacheEnabled(false);
+
+            File image = new File(path);
+            FileOutputStream fileOutputStream = new FileOutputStream(image);
+            int quality = 100;
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+            return image;
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    public static void verifyPermission(Activity activity){
+
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if(permission!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(activity, PERMISSION, REQUEST_EXTERNAL_STORAGE);
+        }
+    }
+
+    public void tomarCapturaDePantalla() {
+
+        // Solicita permiso para acceder a la ubicación de los medios
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_MEDIA_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Solicita el permiso
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_MEDIA_LOCATION}, REQUEST_CODE_PERMISO_UBICACION_MEDIOS);
+            return;
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // Solicita el permiso
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISO_ESCRIBIR_EXTERNO);
+            return;
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.MEDIA_CONTENT_CONTROL) != PackageManager.PERMISSION_GRANTED) {
+            // Solicita el permiso
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISO_ESCRIBIR_EXTERNO);
+            return;
+        }
+
+        // Crea la carpeta dashrolls
+        File directorioDashrolls = new File(Environment.getExternalStorageDirectory(), "dashrolls");
+        if (!directorioDashrolls.exists()) {
+            directorioDashrolls.mkdir();
+        }
+
+
+// Toma la captura de pantalla
+        Bitmap capturaDePantalla = vista.getDrawingCache();
+
+// Genera un nombre para la captura de pantalla
+        int numero = (int) (Math.random() * 1000000);
+        String nombreCaptura = "dash_roll_" + numero + ".png";
+
+// Crea el archivo para la captura de pantalla
+        File archivoCaptura = new File(directorioDashrolls, nombreCaptura);
+        try {
+            archivoCaptura.createNewFile();
+            Log.d("DiceRollerBeta", archivoCaptura.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+
+        }
+
+// Imprime el valor de archivoCaptura
+        Log.d("DiceRollerBeta", archivoCaptura.toString());
+
+// Escribe la captura de pantalla en el archivo
+        try (FileOutputStream fos = new FileOutputStream(archivoCaptura)) {
+            capturaDePantalla.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+// Otorga permisos de escritura al archivo
+        archivoCaptura.setWritable(true);
+
     }
 
 
